47bacf031e84ae7d46034eab0bcba6b40a96c865
Artem <artem.girus212@gmail.com>
2025-04-05 21:22:51 +0300

    1. Исправление в Pydnatic модели. Вместо mail: str поставил mail: EmailStr

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -3,6 +3,6 @@ from pydantic import BaseModel, EmailStr
 # СОЗДАЕМ ВАЛИДАЦИЮ ДАННЫХ КОТОРЫЕ БУДЕМ ПОЛУЧАТЬ ОТ register_users()
 class UserRegistration(BaseModel):
     username: str
-    mail: str
+    mail: EmailStr
     password: str
 
